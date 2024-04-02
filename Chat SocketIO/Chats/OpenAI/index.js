834ab620f9ad3f9f68c4647583a47224d7ebcd47
Angel Mamberto <107218323+AngelMamberto@users.noreply.github.com>
2024-04-02 16:24:51 -0300

    fixing openAI

---
+++ Chat SocketIO/Chats/OpenAI/index.js
@@ -0,0 +1,7 @@
+const fetch = require('node-fetch');
+const helpers = require('../../Helpers/helpers');
+
+// Tu API key de OpenAI
+var openAIKey = '';
+
+console.log("CHUPALA")
---
+++ Chat SocketIO/Helpers/helpers.js
@@ -0,0 +1,34 @@
+/*
+    Reads a file and returns it. If type's null, return a string with the file content
+
+    filePath: relative path to the file you want to read
+    type: optional. Options: JSON -> returns a JSON object
+    attributes: optional. If not null, it will look for these attributes
+        on file and return a JSON object composed by these attributes
+*/
+
+function readFile(filePath, type, attributes = null) {
+    const fs = require("node:fs");
+
+    try {
+        if (type == "JSON") {
+            const jsonData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+
+            if (attributes != null) {
+                // To do
+                /* Look for attributes on file and create a JSON Object */
+            }
+
+            return jsonData;
+        }
+
+        const data = fs.readFileSync(filePath, "utf-8");
+
+        return data;
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+
+module.exports = {readFile};
